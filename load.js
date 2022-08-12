e398533aa3d1425e7b6fcb81b615320881958f8a
azeirf <111145496+azeirf@users.noreply.github.com>
2022-08-12 13:46:39 -0400

    commit

---
+++ load.js
@@ -0,0 +1,6 @@
+$(function(){
+      $("#header").load("https://frieza.neocities.org/header.html"); 
+      $("#navigation").load("https://frieza.neocities.org/navigation.html"); 
+      $("#footer").load("https://frieza.neocities.org/footer.html");
+	  $("#sidebar").load("https://frieza.neocities.org/sidebar.html");
+});
