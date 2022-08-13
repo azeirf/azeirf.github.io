54a3e7665ccd4aea45237a656d26be3f9a69ea25
azeirf <111145496+azeirf@users.noreply.github.com>
2022-08-12 21:31:23 -0400

    layouting

--- load.js
+++ load.js
@@ -1,6 +1,6 @@
 $(function () {
   $("#header").load("/header.html");
-  $("#navigation").load("/navigation.html");
+  $("#navbar").load("/navigation.html");
   $("#footer").load("/footer.html");
   $("#sidebar").load("/sidebar.html");
 });
